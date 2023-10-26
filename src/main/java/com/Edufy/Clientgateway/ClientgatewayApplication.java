9f18d7ed3a1faad580eca6a37a74f56c7647db94
mrNordstrom <mr.nordstrom77@gmail.com>
2023-10-26 10:27:06 +0200

    added API gateway

---
+++ src/main/java/com/Edufy/Clientgateway/ClientgatewayApplication.java
@@ -0,0 +1,12 @@
+package com.Edufy.Clientgateway;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientgatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Edufy/Clientgateway/ClientgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Edufy.Clientgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
